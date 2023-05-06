4f3e07f226c5c772b42519fa347b07890fe5c800
lcRuri <1793185558@qq.com>
2023-05-06 19:57:35 +0800

    文件结构

---
+++ data/data_file.go
@@ -0,0 +1,28 @@
+package data
+
+import "bitcask-go/fio"
+
+const DataFileNameSuffix = ".data"
+
+// DataFile 数据文件
+type DataFile struct {
+	FileId    uint32        //文件id
+	WriteOff  int64         //文件写到了哪个位置
+	IoManager fio.IOManager //io 读写管理
+}
+
+// 打开新的数据文件
+func OpenDataFile(dirpath string, fileId uint32) (*DataFile, error) {
+	return nil, nil
+}
+
+func (df DataFile) ReadLogRecord(offset int64) (*LogRecord, int64, error) {
+	return nil, 0, nil
+}
+func (df *DataFile) Write(buf []byte) error {
+	return nil
+}
+
+func (df *DataFile) Sync() error {
+	return nil
+}
