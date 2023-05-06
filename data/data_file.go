efd2720c32ee879f9fc65ecc7f9011aa7c4b12fd
lcRuri <1793185558@qq.com>
2023-05-06 21:45:13 +0800

    补全OpenDataFile和ReadLogRecord

--- data/data_file.go
+++ data/data_file.go
@@ -1,6 +1,17 @@
 package data
 
-import "bitcask-go/fio"
+import (
+	"bitcask-go/fio"
+	"errors"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"path/filepath"
+)
+
+var (
+	ErrInvalidCRC = errors.New("invalid crc value, log record maybe corrupted")
+)
 
 const DataFileNameSuffix = ".data"
 
@@ -11,18 +22,96 @@ type DataFile struct {
 	IoManager fio.IOManager //io 读写管理
 }
 
-// 打开新的数据文件
-func OpenDataFile(dirpath string, fileId uint32) (*DataFile, error) {
-	return nil, nil
+// OpenDataFile 打开新的数据文件
+func OpenDataFile(dirPath string, fileId uint32) (*DataFile, error) {
+	fileName := filepath.Join(dirPath + fmt.Sprintf("%09d", fileId) + DataFileNameSuffix)
+	//初始化IOManager，就是生成对应文件名的.data文件
+	ioManager, err := fio.NewFileIOManager(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DataFile{
+		FileId:    fileId,
+		WriteOff:  0,
+		IoManager: ioManager,
+	}, nil
 }
 
+// ReadLogRecord 根据offset偏移地址从数据文件中读取LogRecord
 func (df DataFile) ReadLogRecord(offset int64) (*LogRecord, int64, error) {
-	return nil, 0, nil
+	fileSize, err := df.IoManager.Size()
+	if err != nil {
+		return nil, 0, err
+	}
+	//如果LogRecord的长度比默认的LogRecordHeader还小，那么就算出它的实际长度
+	var headerBytes int64 = maxLogRecordHeaderSize
+	if offset+maxLogRecordHeaderSize > fileSize {
+		headerBytes = fileSize - offset
+	}
+
+	//读取header信息
+	headerBuf, err := df.readNBytes(headerBytes, offset)
+	if err != nil {
+		return nil, 0, err
+	}
+	header, headerSize := decodeLogRecordHeader(headerBuf)
+	//当解码的header数据为空，表示读到了文件末尾，返回eof错误
+	if header == nil {
+		return nil, 0, io.EOF
+	}
+	//当解码到了文件末尾
+	if header.crc == 0 && header.keySize == 0 && header.valueSize == 0 {
+		return nil, 0, io.EOF
+	}
+
+	//取出对应的key和value的长度
+	keySize, valueSize := int64(header.keySize), int64(header.valueSize)
+	var recordSize = headerSize + keySize + valueSize
+
+	logRecord := &LogRecord{Type: header.recordType}
+
+	//开始读取用户实际存储的key/value数据
+	if keySize > 0 || valueSize > 0 {
+		kvBuf, err := df.readNBytes(keySize+valueSize, offset+headerSize)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		//解出key和value
+		logRecord.Key = kvBuf[:keySize]
+		logRecord.Value = kvBuf[keySize:]
+	}
+
+	//校验数据的crc是否正确
+	crc := getLogRecordCRC(logRecord, headerBuf[crc32.Size:headerSize])
+	if crc != header.crc {
+		return nil, 0, ErrInvalidCRC
+	}
+
+	return logRecord, recordSize, nil
 }
+
 func (df *DataFile) Write(buf []byte) error {
+	n, err := df.IoManager.Write(buf)
+	if err != nil {
+		return err
+	}
+	//更新writeOff的字段
+	df.WriteOff += int64(n)
 	return nil
 }
 
 func (df *DataFile) Sync() error {
-	return nil
+	return df.IoManager.Sync()
+}
+
+func (df *DataFile) Close() error {
+	return df.IoManager.Close()
+}
+
+func (df *DataFile) readNBytes(n int64, offset int64) (b []byte, err error) {
+	b = make([]byte, n)
+	_, err = df.IoManager.Read(b, offset)
+	return
 }
