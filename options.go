d52d90dc3887e01854c834418678d7bb6269cd2b
lcRuri <1793185558@qq.com>
2023-05-06 19:57:45 +0800

    配置

---
+++ options.go
@@ -0,0 +1,18 @@
+package bitcask_go
+
+type Options struct {
+	DirPath string //数据库目录文件
+
+	DataFileSize int64 //数据文件的大小
+
+	SyncWrites bool //每次写数据是否持久化
+
+	IndexType IndexerType //索引类型
+}
+
+type IndexerType = int8
+
+const (
+	Btree IndexerType = iota + 1
+	ART
+)
