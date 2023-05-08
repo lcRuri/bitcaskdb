064d45a796186268fd0260c18ebab1c1f44e31fd
lcRuri <1793185558@qq.com>
2023-05-08 21:28:28 +0800

    索引迭代器接口

--- index/index.go
+++ index/index.go
@@ -16,6 +16,9 @@ type Indexer interface {
 
 	// Delete 根据key删除对应的索引位置信息
 	Delete(key []byte) bool
+
+	// Iterator 索引迭代器
+	Iterator(reserve bool) Iterator
 }
 
 type IndexType = int8
@@ -48,4 +51,28 @@ type Item struct {
 
 func (ai *Item) Less(bi btree.Item) bool {
 	return bytes.Compare(ai.key, bi.(*Item).key) == -1
+}
+
+// Iterator 通用索引迭代器
+type Iterator interface {
+	// Rewind 重新回到迭代器的起点，即第一个数据的位置
+	Rewind()
+
+	// Seek 根据传入的key查找到第一个大于(小于)等于的目标的key，从这个key开始遍历
+	Seek([]byte)
+
+	// Next 跳转到下一个key
+	Next()
+
+	// Valid 是否有效，即是否已经遍历完了所有的key，用于退出遍历
+	Valid() bool
+
+	// Key 当前遍历位置的Key数据
+	Key() []byte
+
+	// Value 当前遍历位置的Value信息
+	Value() *data.LogRecordPos
+
+	// Close 关闭迭代器，并释放相关资源
+	Close()
 }
