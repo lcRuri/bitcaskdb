f25b3a6c27300cb28c6e2d8e8bd29d65d833c6b5
lcRuri <1793185558@qq.com>
2023-05-08 21:28:42 +0800

    B树迭代器实现测试

--- index/btree_test.go
+++ index/btree_test.go
@@ -56,3 +56,49 @@ func TestBTree_Delete(t *testing.T) {
 	dres := bt.Delete([]byte("a"))
 	assert.True(t, dres)
 }
+
+func TestBTree_Iterator(t *testing.T) {
+	bt1 := NewBTree()
+	//空的btree
+	iter1 := bt1.Iterator(false)
+	t.Log(iter1.Valid())
+	assert.Equal(t, false, iter1.Valid())
+
+	//有数据的btree
+	bt1.Put([]byte("ccde"), &data.LogRecordPos{1, 10})
+	iter2 := bt1.Iterator(false)
+	assert.Equal(t, true, iter2.Valid())
+	t.Log(iter2.Key())
+	t.Log(iter2.Value())
+
+	iter2.Next()
+	t.Log(iter2.Valid())
+
+	//有多条数据
+	bt1.Put([]byte("ccde"), &data.LogRecordPos{1, 10})
+	bt1.Put([]byte("xxxxx"), &data.LogRecordPos{1, 10})
+	bt1.Put([]byte("aassss"), &data.LogRecordPos{1, 10})
+
+	//正向遍历
+	iter3 := bt1.Iterator(false)
+	for iter3.Rewind(); iter3.Valid(); iter3.Next() {
+		t.Log("key = ", string(iter3.Key()))
+	}
+	//反向遍历
+	iter4 := bt1.Iterator(true)
+	for iter4.Rewind(); iter4.Valid(); iter4.Next() {
+		t.Log("key = ", string(iter4.Key()))
+	}
+
+	//测试seek
+	iter5 := bt1.Iterator(false)
+	for iter5.Seek([]byte("cc")); iter5.Valid(); iter5.Next() {
+		t.Log(string(iter5.Key()))
+	}
+
+	iter6 := bt1.Iterator(true)
+	for iter6.Seek([]byte("zz")); iter6.Valid(); iter6.Next() {
+		t.Log(string(iter6.Key()))
+	}
+
+}
