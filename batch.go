0d0e273a9cf99eee824bc771e58c1f5f8de8ef45
lcRuri <1793185558@qq.com>
2023-05-10 14:22:51 +0800

    WriteBatch原子写

---
+++ batch.go
@@ -0,0 +1,165 @@
+package bitcask_go
+
+import (
+	"bitcask-go/data"
+	"encoding/binary"
+	"sync"
+	"sync/atomic"
+)
+
+const nonTransactionSeqNo uint64 = 0
+
+var txnFinKey = []byte("txn-fin")
+
+type WriteBatch struct {
+	options       WriteBatchOptions
+	mu            *sync.Mutex
+	db            *DB
+	pendingWrites map[string]*data.LogRecord //暂存用户写入的数据
+}
+
+// NewWriteBatch 初始化
+func (db *DB) NewWriteBatch(opts WriteBatchOptions) *WriteBatch {
+	return &WriteBatch{
+		options:       opts,
+		mu:            new(sync.Mutex),
+		db:            db,
+		pendingWrites: map[string]*data.LogRecord{},
+	}
+}
+
+// Put 批量写数据
+func (wb *WriteBatch) Put(key []byte, value []byte) error {
+	if len(key) == 0 {
+		return ErrKeyIsEmpty
+	}
+
+	wb.mu.Lock()
+	defer wb.mu.Unlock()
+
+	//暂存logRecord
+	logRecord := &data.LogRecord{Key: key, Value: value}
+	wb.pendingWrites[string(key)] = logRecord
+	return nil
+}
+
+// Delete 删除数据
+func (wb *WriteBatch) Delete(key []byte) error {
+	if len(key) == 0 {
+		return ErrKeyIsEmpty
+	}
+
+	wb.mu.Lock()
+	defer wb.mu.Unlock()
+
+	//数据不存在则直接返回
+	logRecordPos := wb.db.index.Get(key)
+	if logRecordPos == nil {
+		//删除暂存的数据
+		if wb.pendingWrites[string(key)] != nil {
+			delete(wb.pendingWrites, string(key))
+		}
+		return nil
+	}
+
+	//暂存logRecord
+	//将type标记为deleted
+	logRecord := &data.LogRecord{Key: key, Type: data.LogRecordDeleted}
+	wb.pendingWrites[string(key)] = logRecord
+	return nil
+}
+
+// Commit 提交事务 将暂存的数据写到数据文件 更新索引
+func (wb *WriteBatch) Commit() error {
+	wb.mu.Lock()
+	wb.mu.Unlock()
+
+	if len(wb.pendingWrites) == 0 {
+		return nil
+	}
+
+	if len(wb.pendingWrites) > wb.options.MaxBatchNum {
+		return ErrExceedMaxBatchNum
+	}
+
+	//加锁保证事务提交的串行化
+	wb.mu.Lock()
+	defer wb.mu.Unlock()
+
+	//实际写入数据
+	//获取当前最新事务序列号
+	seqNo := atomic.AddUint64(&wb.db.seqNo, 1)
+
+	//开始写数据到数据文件
+	position := make(map[string]*data.LogRecordPos)
+	for _, record := range wb.pendingWrites {
+		//暂存单条数据的索引信息
+		logRecordPos, err := wb.db.appendLogRecord(&data.LogRecord{
+			Key:   logRecordKeyWithSeq(record.Key, seqNo),
+			Value: record.Value,
+			Type:  record.Type,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		position[string(record.Key)] = logRecordPos
+
+	}
+
+	//写一条标识事务完成的数据
+	finishedRecord := &data.LogRecord{
+		Key:  logRecordKeyWithSeq(txnFinKey, seqNo),
+		Type: data.LogRecordTxnFinished,
+	}
+
+	if _, err := wb.db.appendLogRecord(finishedRecord); err != nil {
+		return err
+	}
+
+	//根据配置决定是否持久化
+	if wb.options.SyncWrites && wb.db.activeFile != nil {
+		if err := wb.db.activeFile.Sync(); err != nil {
+			return err
+		}
+	}
+
+	//更新对应的内存索引
+	for _, record := range wb.pendingWrites {
+		pos := position[string(record.Key)]
+
+		//type是正常的数据
+		if record.Type == data.LogRecordNormal {
+			wb.db.index.Put(record.Key, pos)
+		}
+		//???
+		if record.Type == data.LogRecordDeleted {
+			wb.db.index.Delete(record.Key)
+		}
+	}
+
+	//清空暂存的数据 方便下一次commit
+	wb.pendingWrites = make(map[string]*data.LogRecord)
+
+	return nil
+}
+
+// key+Seq Number编码 将事务序列号编码到key前面
+func logRecordKeyWithSeq(key []byte, seqNo uint64) []byte {
+	seq := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutUvarint(seq[:], seqNo)
+
+	encKey := make([]byte, n+len(key))
+	copy(encKey[:n], seq[:n])
+	copy(encKey[n:], key)
+
+	return encKey
+}
+
+// 解析logRecord的key，获取实际的key和事务序列号
+func parseLogRecordKey(key []byte) ([]byte, uint64) {
+	seqNo, n := binary.Uvarint(key)
+	realKey := key[n:]
+	return realKey, seqNo
+}
