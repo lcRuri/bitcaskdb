0c57cb8e55be2486f5775a250d7b80ad3f86be78
lcRuri <1793185558@qq.com>
2023-05-14 23:13:57 +0800

    拷贝数据目录

--- utils/file.go
+++ utils/file.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"io/fs"
+	"os"
 	"path/filepath"
+	"strings"
 	"syscall"
 )
 
@@ -37,4 +39,45 @@ func AvailableDiskSize() (uint64, error) {
 	}
 
 	return stat.Bavail * uint64(stat.Bsize), nil
+}
+
+// CopyDir 拷贝数据目录
+func CopyDir(src, dest string, exclude []string) error {
+	//目标不存在则创建
+	if _, err := os.Stat(dest); os.IsNotExist(err) {
+		if err := os.MkdirAll(dest, os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	return filepath.Walk(src, func(path string, info fs.FileInfo, err error) error {
+		fileName := strings.Replace(path, src, "", 1)
+
+		if fileName == "" {
+			return nil
+		}
+
+		//看这个文件是否在排除列表当中
+		for _, e := range exclude {
+			matched, err := filepath.Match(e, info.Name())
+			if err != nil {
+				return err
+			}
+			if matched {
+				return nil
+			}
+		}
+
+		//如果是一个文件夹
+		if info.IsDir() {
+			return os.MkdirAll(filepath.Join(dest, fileName), info.Mode())
+		}
+
+		//常规文件 读取出来
+		data, err := os.ReadFile(filepath.Join(src, fileName))
+		if err != nil {
+			return err
+		}
+		return os.WriteFile(filepath.Join(dest, fileName), data, info.Mode())
+	})
 }
