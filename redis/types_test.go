7377647713a844c1ad41fc6e687501191548f83d
lcRuri <1793185558@qq.com>
2023-05-20 15:53:36 +0800

    redisString类型测试

---
+++ redis/types_test.go
@@ -0,0 +1,55 @@
+package redis
+
+import (
+	bitcask "bitcask-go"
+	"bitcask-go/utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestRedisDataStructure_Get(t *testing.T) {
+	opts := bitcask.DefaultOptions
+	rds, err := NewRedisDataStructure(opts)
+	assert.Nil(t, err)
+
+	err = rds.Set(utils.GetTestKey(1), 0, utils.RandomValue(100))
+	assert.Nil(t, err)
+	err = rds.Set(utils.GetTestKey(2), time.Second*5, utils.RandomValue(100))
+	assert.Nil(t, err)
+
+	val1, err := rds.Get(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.NotNil(t, val1)
+
+	val2, err := rds.Get(utils.GetTestKey(2))
+	assert.Nil(t, err)
+	assert.NotNil(t, val2)
+
+	_, err = rds.Get(utils.GetTestKey(33))
+	assert.Equal(t, bitcask.ErrKeyNotFound, err)
+}
+
+func TestRedisDataStructure_Del_Type(t *testing.T) {
+	opts := bitcask.DefaultOptions
+	rds, err := NewRedisDataStructure(opts)
+	assert.Nil(t, err)
+
+	// del
+	err = rds.Del(utils.GetTestKey(11))
+	assert.Nil(t, err)
+
+	err = rds.Set(utils.GetTestKey(1), 0, utils.RandomValue(100))
+	assert.Nil(t, err)
+
+	// type
+	typ, err := rds.Type(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.Equal(t, String, typ)
+
+	err = rds.Del(utils.GetTestKey(1))
+	assert.Nil(t, err)
+
+	_, err = rds.Get(utils.GetTestKey(1))
+	assert.Equal(t, bitcask.ErrKeyNotFound, err)
+}
