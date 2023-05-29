fa5e2075c7a36491b08738a035a215463038c399
lcRuri <1793185558@qq.com>
2023-05-29 19:53:31 +0800

    Hash结构编码

--- redis/types.go
+++ redis/types.go
@@ -80,3 +80,143 @@ func (rds *RedisDataStructure) Get(key []byte) ([]byte, error) {
 
 	return encValue[index:], err
 }
+
+// ======================= Hash 数据结构 =======================
+
+func (rds *RedisDataStructure) HSet(key, field, value []byte) (bool, error) {
+	// 查找对应的元数据,不存在则新建
+	meta, err := rds.findMetadata(key, Hash)
+	if err != nil {
+		return false, err
+	}
+
+	// 构造 Hash 数据部分的key
+	hk := &hashInternalKey{
+		key:     key,
+		version: meta.version,
+		field:   field,
+	}
+
+	enckey := hk.encode()
+
+	//先查找数据是否存在
+	var exist = true
+	if _, err := rds.db.Get(key); err == bitcask_go.ErrKeyNotFound {
+		exist = false
+	}
+
+	wb := rds.db.NewWriteBatch(bitcask_go.DefaultWriteBatchOptions)
+	//如果不存在，更新元数据
+	if !exist {
+		meta.size++
+		_ = wb.Put(key, meta.encode())
+	}
+
+	_ = wb.Put(enckey, value)
+
+	if err = wb.Commit(); err != nil {
+		return false, err
+	}
+
+	return !exist, nil
+
+}
+
+func (rds *RedisDataStructure) HGet(key, field []byte) ([]byte, error) {
+	meta, err := rds.findMetadata(key, Hash)
+	if err != nil {
+		return nil, err
+	}
+	//元数据的size为0，说明这个key压根没存数据
+	if meta.size == 0 {
+		return nil, nil
+	}
+
+	// 构造 Hash 数据部分的key
+	hk := &hashInternalKey{
+		key:     key,
+		version: meta.version,
+		field:   field,
+	}
+
+	return rds.db.Get(hk.encode())
+}
+
+func (rds *RedisDataStructure) HDel(key, field []byte) (bool, error) {
+	meta, err := rds.findMetadata(key, Hash)
+	if err != nil {
+		return false, err
+	}
+	//元数据的size为0，说明这个key压根没存数据
+	if meta.size == 0 {
+		return false, nil
+	}
+
+	// 构造 Hash 数据部分的key
+	hk := &hashInternalKey{
+		key:     key,
+		version: meta.version,
+		field:   field,
+	}
+
+	encKey := hk.encode()
+
+	//查看是否存在,存在删除才为true，否则是false
+	var exist = true
+	if _, err := rds.db.Get(encKey); err == bitcask_go.ErrKeyNotFound {
+		exist = false
+	}
+
+	if exist {
+		wb := rds.db.NewWriteBatch(bitcask_go.DefaultWriteBatchOptions)
+		meta.size--
+		_ = wb.Put(key, meta.encode())
+		_ = wb.Delete(key)
+
+		if err = wb.Commit(); err != nil {
+			return false, err
+		}
+
+	}
+
+	return exist, nil
+}
+
+func (rds *RedisDataStructure) findMetadata(key []byte, dataType RedisDataType) (*metadata, error) {
+	metaBuf, err := rds.db.Get(key)
+	if err != nil && err != bitcask_go.ErrKeyNotFound {
+		return nil, err
+	}
+
+	var meta *metadata
+	var exist = true
+	if err == bitcask_go.ErrKeyNotFound {
+		exist = false
+	} else {
+		meta = decodeMetadata(metaBuf)
+		//判断数据类型
+		if meta.dataType != Hash {
+			return nil, ErrWrongTypeOperation
+		}
+		//判断过期时间
+		if meta.expire != 0 && meta.expire <= time.Now().UnixNano() {
+			exist = false
+		}
+	}
+
+	if !exist {
+		meta = &metadata{
+			dataType: Hash,
+			expire:   0,
+			version:  time.Now().UnixNano(),
+			size:     0,
+		}
+
+		if dataType == List {
+			meta.head = initialListMark
+			meta.tail = initialListMark
+		}
+	}
+
+	return meta, nil
+}
